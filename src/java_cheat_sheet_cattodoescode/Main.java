85fd9b3fc8023662d898e40a2f9060224915c6e3
CattoDoesCode <moralespax1@gmail.com>
2022-03-13 14:58:10 +0800

    Initial commit

--- src/java_cheat_sheet_cattodoescode/Main.java
+++ src/java_cheat_sheet_cattodoescode/Main.java
@@ -3,6 +3,6 @@ package java_cheat_sheet_cattodoescode;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here 
+	    System.out.println("Hello, World!");
     }
 }
